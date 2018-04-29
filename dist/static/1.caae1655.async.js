0ed321920d9fddfb5ba263be346d7d5a9fb3090c
zylee <zylee005@163.com>
2018-04-29 12:17:14 +0800

    放开dist目录,一起提交

---
+++ dist/static/1.caae1655.async.js
@@ -0,0 +1,1 @@
+webpackJsonp([1],{"04qB":function(e,t,n){"use strict";function c(){return i}Object.defineProperty(t,"__esModule",{value:!0});var r=n("GiK3"),a=n.n(r),o=n("S6G3"),u=(n.n(o),n("7xWd")),i=(n.n(u),a.a.createElement(u.Redirect,{to:"/assg"}));t.default=Object(o.connect)()(c)}});
---
+++ dist/static/8.54fe396d.async.js
@@ -0,0 +1,1 @@
+webpackJsonp([8],{CBwK:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var u=t("GiK3"),a=t.n(u),r=a.a.createElement("div",null,"404 page ");n.default=function(){return r}}});
